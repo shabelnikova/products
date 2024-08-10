5567d7a9265f130b6a7bdcfa48fe8353b0574a8f
shabelnikova <shabelnikova@gmail.com>
2024-08-10 20:14:05 +0300

    implemented admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+"use client";
+import React, {useState} from 'react';
+import initialProducts from "@/data/products";
+import AdminProduct from "@/components/AdminProduct";
+import {Product} from "@/types/product";
 
 const Page = () => {
+    const [products, setProducts] = useState<Product[]>(initialProducts);
+
+    const handleDelete = (id: string) => {
+        const updatedProducts = products.filter(product => product.id !== id);
+        setProducts(updatedProducts); // Обновляем состояние
+    };
+
     return (
-        <div>
-            ADMIN PAGE
-        </div>
+        <main className="mx-auto max-w-screen-2xl py-4 w-4/5 flex flex-col gap-4">
+            <button className="
+            py-10 px-14 text-xl
+            font-semibold border bg-gray-300
+            rounded-lg hover:bg-gray-400/60
+            ">
+                Add new product
+            </button>
+            {products.map((product) => (
+                <AdminProduct
+                    key={product.id}
+                    product={product}
+                    onDelete={handleDelete}
+                />
+            ))}
+        </main>
     );
 };
 
---
+++ src/components/AdminProduct.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { FaRegTrashCan } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
+import {Product} from "@/types/product";
+
+const AdminProduct: React.FC<{ product: Product; onDelete: (id: string) => void }> = ({product, onDelete}) => {
+    const { image, id, name, price, description, releaseDate } = product;
+
+    return (
+        <div className="flex gap-4 p-4 border border-gray-300 rounded justify-between">
+            <img className="w-28" src={image} alt={name}/>
+            <p>Id: {id}</p>
+            <p>Name: {name}</p>
+            <p>Price: {price}</p>
+            <p>Release date: {releaseDate.toLocaleDateString()}</p>
+            <FaEdit className="text-xl cursor-pointer"/>
+            <FaRegTrashCan className="text-xl cursor-pointer" onClick={() => onDelete(id)}/>
+        </div>
+    );
+};
+
+export default AdminProduct;

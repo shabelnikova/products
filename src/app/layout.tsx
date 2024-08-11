1b941f43f18ea56d76e02dbe5aa70a4f30d43493
shabelnikova <shabelnikova@gmail.com>
2024-08-11 18:05:25 +0300

    remove Head component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,11 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <Head>
+    <head>
         <title>{metadata.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/favicon.ico" />
-    </Head>
+    </head>
       <body className={inter.className}>
       <Header/>
       {children}

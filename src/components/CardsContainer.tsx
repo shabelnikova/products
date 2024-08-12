53be39f0546b469513e34b1d70da65bf458bcc10
shabelnikova <shabelnikova@gmail.com>
2024-08-12 20:39:50 +0300

    fix styles

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -8,8 +8,9 @@ const CardsContainer = ({ products }: { products: Product[] }) => {
     }
     return (
         <>
-            <h1 className="text-2xl font-bold pl-8">Our products</h1>
-            <div className="py-10 mx-auto grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10 bg-light-gray-cold">
+            <h1 className="text-black text-2xl font-bold pl-8">Our products</h1>
+            <div className="text-black py-10 mx-auto grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10
+            bg-light-gray-cold">
                 {products.map((el) => (
                     <Card
                         key={el.id}

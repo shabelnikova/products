0ef27f6a6acef3336180783c61f2c76b94e1caa1
shabelnikova <shabelnikova@gmail.com>
2024-08-13 15:48:45 +0300

    add file vercel.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,47 @@
 import { MongoClient } from 'mongodb';
 import CardsContainer from "@/components/CardsContainer";
-import {Product} from "@/types/product";
+import { Product } from "@/types/product";
 
 const uri: string = process.env.MONGODB_URI!;
 const dbName: string = "products";
 const collectionName: string = "items";
-async function fetchProducts(): Promise<Product[]> {
+
+let cachedClient: MongoClient | null = null;
+
+async function connectToDatabase(): Promise<MongoClient> {
+    if (cachedClient) {
+        return cachedClient;
+    }
     const client = new MongoClient(uri);
-    await client.connect();
-    const database = client.db(dbName);
-    const collection = database.collection(collectionName);
-
-    const products = await collection.find({}).toArray();
-    await client.close();
-
-    const currentDate = new Date();
-
-    return products
-        .map(product => ({
-            id: product._id.toString(),
-            image: product.image,
-            name: product.name,
-            price: product.price,
-            description: product.description,
-            releaseDate: new Date(product.releaseDate),
-        }))
-        .filter(product => product.releaseDate <= currentDate);
+    cachedClient = await client.connect();
+    return cachedClient;
 }
 
+async function fetchProducts(): Promise<Product[]> {
+    try {
+        const client = await connectToDatabase();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const products = await collection.find({}).toArray();
+
+        const currentDate = new Date();
+
+        return products
+            .map(product => ({
+                id: product._id.toString(),
+                image: product.image,
+                name: product.name,
+                price: product.price,
+                description: product.description,
+                releaseDate: new Date(product.releaseDate),
+            }))
+            .filter(product => product.releaseDate <= currentDate);
+    } catch (error) {
+        console.error("Failed to fetch products:", error);
+        return [];
+    }
+}
 
 export default async function Home() {
     const products = await fetchProducts();
